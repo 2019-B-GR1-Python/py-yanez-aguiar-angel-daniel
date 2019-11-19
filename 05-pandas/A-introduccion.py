2a4d1bfb2b9a8e17626c6b1c7f108c7c9e48dbc2
AngelDYaAg <angel_0930@hotmail.es>
2019-11-19 09:01:07 -0500

    pandas

---
+++ 05-pandas/A-introduccion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+nombre = "Angel"
+numero = 3
+lista = [1,2,3]
+tupla = (4,5,6)
+diccionario = {"nombre":"A","cedula":"1"}
