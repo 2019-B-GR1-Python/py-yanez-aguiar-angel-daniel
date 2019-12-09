07aa76b78ba342fdaba88a66c6c2b73a6b824ae5
AngelDYaAg <angel_0930@hotmail.es>
2019-12-09 12:25:13 -0500

    Proyecto y deber

--- 05-pandas/E_Output_Data.py
+++ 05-pandas/E_Output_Data.py
@@ -9,7 +9,8 @@ import os
 import pandas as pd
 import numpy as np
 import sqlite3
-path_guardado_bin = "C://Users//USRBET//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//artwork_data_completo.pickle"
+import xlsxwriter
+path_guardado_bin = "C://Users//Angel//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//artwork_data_completo.pickle"
 df5 = pd.read_pickle(path_guardado_bin)
 df = df5.iloc[49980:50019,:].copy()
 # tipos archivos
@@ -18,12 +19,12 @@ df = df5.iloc[49980:50019,:].copy()
 # SQL
 
 ### EXCEL ###
-path_guardado = "C://Users//USRBET//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_completo.xlsx"
+path_guardado = "C://Users//Angel//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_completo.xlsx"
 df.to_excel(path_guardado,index=False)
 columnas = ['artist','title','year']
-df.to_excel(path_guardado, columnas)
+df.to_excel(path_guardado, columns=columnas)
 ### multiples hojas de trabajo ###
-path_multiple = "C://Users//USRBET//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_multiple.xlsx"
+path_multiple = "C://Users//Angel//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_multiple.xlsx"
 writer = pd.ExcelWriter(path_multiple, engine='xlsxwriter')
 df.to_excel(writer,sheet_name = 'Primera')
 df.to_excel(writer,sheet_name = 'Segunda',index=True)
@@ -31,7 +32,7 @@ df.to_excel(writer,sheet_name = 'Tercera',columns=columnas)
 writer.save()
 
 num_artistas = df['artist'].value_counts()
-path_colores = "C://Users//USRBET//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_colores.xlsx"
+path_colores = "C://Users//Angel//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_colores.xlsx"
 writer = pd.ExcelWriter(path_colores, engine='xlsxwriter')
 num_artistas.to_excel(writer, sheet_name='Artistas')
 
@@ -47,5 +48,14 @@ formato_artistas = {"type":"2_color_scale",
 hoja_artistas.conditional_format(rango_celdas,formato_artistas)
 
 writer.save()
+
+workbook = xlsxwriter.Workbook('C://Users//Angel//Documents//GitHub//py-yanez-aguiar-angel-daniel//05-pandas//data//mi_df_colores.xlsx')
+worksheet = workbook.add_worksheet()
+data = num_artistas.values
+worksheet.write_column('B2',data)
+chart = workbook.add_chart({'type':'line'})
+chart.add_series({'values':'=Sheet1!$B$2:$B$4'})
+worksheet.insert_chart('C1', chart)
+workbook.close()
 
 
